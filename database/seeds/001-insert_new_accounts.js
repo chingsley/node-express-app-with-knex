9fd05748435412e392390bcf24088413466840bd
chingsley <eneja.kc@gmail.com>
2020-01-11 19:31:31 +0100

    before bulk seed

---
+++ database/seeds/001-insert_new_accounts.js
@@ -0,0 +1,26 @@
+function range(start, end) {
+  return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
+}
+
+let seedAccounts = [];
+for (i of range(0, 999)) {
+  seedAccounts[i] = {
+    "accountName": "ISA AYO OBI ",
+    "accountNumber": `465432${i}`,
+    "phoneNumber": `2306123456${i}`,
+    "branchCode": `99917010${i}`,
+    "accountStatus": 1,
+    "accountType": 1,
+    "bvn": `67665232${i}`
+  }
+}
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('new_accounts')
+    .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('new_accounts').insert(seedAccounts);
+    });
+};
